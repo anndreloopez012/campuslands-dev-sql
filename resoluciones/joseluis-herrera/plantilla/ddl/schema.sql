ced0c93e56560e50ed26c98fb312b09dd45d2599
Jose-langL <totherrerajoseluis@gmail.com>
2026-06-09 07:26:15 -0600

    fix(resoluciones) Se corrigio error del ejercicio

--- resoluciones/joseluis-herrera/plantilla/ddl/schema.sql
+++ resoluciones/joseluis-herrera/plantilla/ddl/schema.sql
@@ -11,13 +11,13 @@ CREATE TABLE Peliculas (
     descripcion TEXT NOT NULL,
     duracion INTEGER NOT NULL CHECK (duracion > 0),
     idioma TEXT NOT NULL
-)
+);
 
 CREATE TABLE Salas (
     id_sala INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL UNIQUE,
     cantidad_asientos INTEGER NOT NULL CHECK (cantidad_asientos > 0)
-)
+);
 
 CREATE TABLE Funciones (
     id_funcion INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,7 +27,7 @@ CREATE TABLE Funciones (
 
     FOREIGN KEY (id_pelicula) REFERENCES Peliculas(id_pelicula),
     FOREIGN KEY (id_sala) REFERENCES Salas(id_sala)
-)
+);
 
 CREATE TABLE Boletos (
     id_boleto INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,5 +36,5 @@ CREATE TABLE Boletos (
     precio REAL NOT NULL CHECK (precio > 0),
     
     FOREIGN KEY (id_funcion) REFERENCES Funciones(id_funcion)
-)
+);
 
