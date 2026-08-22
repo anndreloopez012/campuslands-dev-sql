75433a1fb3a3615a4d56b4627e4244f11b14689d
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-22 06:29:19 -0600

    feat(solicitudes-sql): resolver ejercicio 097

---
+++ resoluciones/jakelin-quino/solicitudes-sql/ejercicio-097/dml/operaciones.sql
@@ -0,0 +1,8 @@
+PRAGMA foreign_keys = ON;
+
+-- Ejercicio 097: Liga MOBA
+INSERT INTO partidas (jugador_id, heroe, kda_ratio) VALUES (1, 'Azir', 12.0);
+
+UPDATE jugadores SET rol = 'Capitan Mid' WHERE id = 1;
+
+DELETE FROM equipos WHERE id = 2 AND NOT EXISTS (SELECT 1 FROM jugadores WHERE equipo_id = equipos.id);
