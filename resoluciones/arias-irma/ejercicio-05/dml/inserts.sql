a92cbc7df9d42276b99d4c9b279911e6c664766c
irm338 <yanehtarias12@gmail.com>
2026-08-24 21:43:52 -0600

    feat(sql): resolver ejercicio 05 inventario de bicicletas completo

---
+++ resoluciones/arias-irma/ejercicio-05/dml/inserts.sql
@@ -0,0 +1,5 @@
+PRAGMA foreign_keys = ON;
+
+-- Ejercicio 05: Inventario de Bicicletas
+-- Inserte aqui los datos base.
+-- Debe incluir datos suficientes para probar todas las consultas.
---
+++ resoluciones/arias-irma/ejercicio-05/dml/operaciones.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys = ON;
+
+-- Ejercicio 05: Inventario de Bicicletas
+-- Agregue aqui INSERT, UPDATE y DELETE adicionales.
+
+-- INSERT validos:
+
+-- UPDATE validos con WHERE:
+
+-- DELETE controlados con WHERE:
+
+-- Casos que deben fallar por restricciones, dejelos comentados:
+-- INSERT INTO ...;
