1f60e06d989928bdc071d98e4aafaafc6bb83b42
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-31 06:31:06 -0600

    feat(solicitudes-sql): resolver ejercicio 015 (#221)

---
+++ resoluciones/jakelin-quino/solicitudes-sql/ejercicio-015/dml/operaciones.sql
@@ -0,0 +1,8 @@
+PRAGMA foreign_keys = ON;
+
+-- Ejercicio 015: Laboratorio Quimico
+INSERT INTO ensayos (muestra_id, reactivo_id, resultado) VALUES (1, 2, 'Precipitado Blanco');
+
+UPDATE reactivos SET pureza_pct = 99.5 WHERE id = 1;
+
+DELETE FROM muestras WHERE id = 2 AND NOT EXISTS (SELECT 1 FROM ensayos WHERE muestra_id = muestras.id);
