0c07eb761219db2c0445c49174b05f71bc1b45b2
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-09-10 09:23:37 -0600

    feat(solicitudes-sql): resolver ejercicio 066 (#330)

---
+++ resoluciones/jakelin-quino/solicitudes-sql/ejercicio-066/dml/operaciones.sql
@@ -0,0 +1,8 @@
+PRAGMA foreign_keys = ON;
+
+-- Ejercicio 066: Hotel Gamers
+INSERT INTO reservas (huesped_id, habitacion_id, noches) VALUES (1, 2, 1);
+
+UPDATE habitaciones SET precio_noche = 90.00 WHERE id = 2;
+
+DELETE FROM huespedes WHERE id = 2 AND NOT EXISTS (SELECT 1 FROM reservas WHERE huesped_id = huespedes.id);
